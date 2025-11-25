1601d93b7b8336fcb72e1b0b4ac310f1528a1b48
lildibbb <adib.asyraaf760@gmail.com>
2025-11-25 21:23:24 +0800

    update config

--- apps/backend/src/config/configuration.ts
+++ apps/backend/src/config/configuration.ts
@@ -30,4 +30,14 @@ export default () => ({
       },
     },
   },
+
+  database: {
+    type: process.env.DB_TYPE,
+    url: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!, 10) || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    name: process.env.DB_NAME,
+  },
 });
